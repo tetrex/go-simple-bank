4e3da785720dcf08d86afb796eedcef1972bf181
harshR <42332761+tetrex@users.noreply.github.com>
2023-12-28 13:34:58 +0530

    feat: added account test

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tetrex/backend-masterclass-go/util"
+)
+
+func createRandomUser(t *testing.T) User {
+	args := CreateUserParams{
+		Username:       util.RandomOwner(),
+		HashedPassword: "secret",
+		FullName:       util.RandomOwner(),
+		Email:          util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, args.Username, user.Username)
+	require.Equal(t, args.HashedPassword, user.HashedPassword)
+	require.Equal(t, args.FullName, user.FullName)
+	require.Equal(t, args.Email, user.Email)
+
+	require.NotZero(t, user.Username)
+	require.NotZero(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	randomUser := createRandomUser(t)
+	user, err := testQueries.GetUser(context.Background(), randomUser.Username)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, randomUser.Username, user.Username)
+	require.Equal(t, randomUser.HashedPassword, user.HashedPassword)
+	require.Equal(t, randomUser.FullName, user.FullName)
+	require.Equal(t, randomUser.Email, user.Email)
+	require.WithinDuration(t, randomUser.CreatedAt, user.CreatedAt, time.Second)
+}
